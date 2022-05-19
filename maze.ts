8410dce59ea9da3aaa4e547d16e391721a7e8801
niclei25 <nikaleifer@gmail.com>
2022-05-19 08:51:30 -0400

    created maze

--- maze.ts
+++ maze.ts
@@ -1,1 +1,17 @@
 console.log("Hi! This is our maze... it'll be here soon!")
+
+class Maze {
+
+    length : number;
+    width : number;
+
+    constructor(length : number, width : number) {
+        this.length = length;
+        this.width = width;
+    }
+
+    createMaze() {
+        new Array(length)
+    }
+
+}
