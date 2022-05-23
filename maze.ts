26b9a91cc0196baa292bb83ff93541c745d9ce07
niclei25 <nikaleifer@gmail.com>
2022-05-23 09:01:08 -0400

    progress on the maze - not working yet

--- maze.ts
+++ maze.ts
@@ -11,7 +11,33 @@ class Maze {
     }
 
     createMaze() {
-        let maze = new Array(length);
+        let maze = new Array(this.length);
+        maze.forEach( (n, index : number) => {
+            maze[index] = new Array(this.width);
+        });
+    
+        for(let k = 1; k < this.width; k++) {
+            maze[0][k] = "wall";
+            maze[this.length - 1][k] = "wall";
+        }
+
+        // for(let x = 0; x <= this.length; x++) {
+        //     maze[x][0] = "wall";
+        //     maze[x][this.length - 1] = "wall";
+        // }
+        for (let a = 0; a <= this.length; a++) {
+            for (let b = 0;  b <= this.width; b + 2) {
+                maze[a][b] = "wall";
+            }
+        }
+
+        maze[0][this.width - 1] = "EXIT";
+        maze[this.length - 1][0] = "ENTER";
+
+        console.log(maze)
     }
 
 }
+
+let m = new Maze(5, 7)
+m.createMaze()
