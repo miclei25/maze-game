89924c013edc10f6df78b354b8e9162b78407a85
niclei25 <nikaleifer@gmail.com>
2022-05-24 08:51:30 -0400

    maze changes

--- maze.js
+++ maze.js
@@ -7,25 +7,30 @@ class Maze {
     }
     createMaze() {
         let maze = new Array(this.length);
-        maze.forEach((n, index) => {
-            maze[index] = new Array(this.width);
-        });
-        for (let k = 1; k < this.width; k++) {
+        // console.log(maze)
+        for (let n = 0; n < this.length; n++) {
+            maze[n] = new Array(this.width);
+        }
+        console.log(maze);
+        // console.log(maze[0])
+        for (let k = 0; k < this.width; k++) {
             maze[0][k] = "wall";
             maze[this.length - 1][k] = "wall";
         }
-        // for(let x = 0; x <= this.length; x++) {
-        //     maze[x][0] = "wall";
-        //     maze[x][this.length - 1] = "wall";
-        // }
-        for (let a = 0; a <= this.length; a++) {
-            for (let b = 0; b <= this.width; b + 2) {
+        console.log(maze);
+        for (let a = 1; a < this.length - 1; a++) {
+            for (let b = 0; b < this.width; b += 2) {
                 maze[a][b] = "wall";
             }
         }
+        console.log(maze);
         maze[0][this.width - 1] = "EXIT";
         maze[this.length - 1][0] = "ENTER";
+        ////////////////////////////////////////////////
         console.log(maze);
+        for (let x = 0; x < this.length; x++) {
+            console.log(maze[x]);
+        }
     }
 }
 let m = new Maze(5, 7);
