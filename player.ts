a821e48d710bf7ff60b26a21b0db2b287c290f61
anaajesh <anaghaajesh2010@gmail.com>
2022-05-25 08:33:20 -0400

    drawing player

--- player.ts
+++ player.ts
@@ -1,3 +1,6 @@
+let canvas = document.querySelector("canvas") as HTMLCanvasElement;
+let ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
 class Actor{
 
     x : number;
@@ -6,6 +9,13 @@ class Actor{
     constructor(x: number, y : number) {
         this.x = x;
         this.y = y;
+    }
+
+    draw() : void {
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,10, 0 , Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
     }
 }
 
