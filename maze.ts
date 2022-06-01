c7d22ab8555b0b8c71be14aec3e43f9fb2a6caa8
niclei25 <nikaleifer@gmail.com>
2022-06-01 09:24:09 -0400

    working on the maze

--- maze.ts
+++ maze.ts
@@ -11,33 +11,23 @@ class Maze {
     }
 
     createMaze() {
+        // creates initial array - first just rows, then makes each of the rows full of columns
         let maze : any[][] = new Array(this.length);
-        // console.log(`before first loop: ${maze}`)
         for (let n = 0; n < this.length; n++) {
             maze[n] = new Array(this.width);
         }
-        // console.log(`after first loop: ${maze}`)
-        // console.log(`first row after first loop: ${maze}`)
     
+        // makes the first and last rows walls
         for (let k = 0; k < this.width; k++) {
             maze[0][k] = "wall";
             maze[this.length - 1][k] = "wall";
         }
 
+        // makes the first and last columns walls
         for (let k = 1; k < this.length - 1; k += 2) {
             maze[k][0] = "wall";
             maze[k][this.width - 1] = "wall";
         }
-
-        // console.log(`after second loop: ${maze}`)
-
-        for (let a = 2; a < this.length - 2; a += 2) {
-            for (let b = 0;  b < this.width; b += 2) {
-                maze[a][b] = "wall";
-            }
-        }
-
-        // console.log(`after third loop: ${maze}`)
 
         maze[0][this.width - 1] = "EXIT";
         maze[this.length - 1][0] = "ENTER";
@@ -47,10 +37,63 @@ class Maze {
         for (let x = 0; x < this.length; x++) {
             console.log("each row after everything:" + maze[x]);
         }
+        ////////////////////////////////////////////////
+        let toBeWall : boolean = true
 
+        let start = this.randomPoint(toBeWall);
+        maze[start[0]][start[1]] = "wall";
+        const N =  2;
+        const S = -2;
+        const E =  2;
+        const W = -2;
+        let directions = [N, S, E, W].sort() // algorithm to sort randomly if time
+        for (let d of directions) {
+
+            let nx = start[0]
+            let ny = start[1]
+            if (d == E || d == W) {
+                nx += d
+            } else {
+                ny += d
+            }
+
+            if (ny > 0 && ny < this.length - 1  && nx > 0 && nx < this.length && maze[nx][ny] !== "wall") {
+
+            }
+
+            // nx, ny = cx + DX[direction], cy + DY[direction]
+
+            // if ny.between?(0, grid.length-1) && nx.between?(0, grid[ny].length-1) && grid[ny][nx] == 0
+            //     grid[cy][cx] |= direction
+            //     grid[ny][nx] |= OPPOSITE[direction]
+            //     carve_passages_from(nx, ny, grid)
+        };
+
+    }
+
+    randomPoint(toBeWall : boolean) {
+        let x = Math.random() * (this.width - 2) + 1;
+        let y = Math.random() * (this.length - 2) + 1;
+
+        if (toBeWall) {
+            if (x % 2 == 0) {
+                x = (x - 2) / 2;
+            } else {
+                x = (x - 1) / 2;
+            }
+            if (y % 2 == 0) {
+                y = (y - 2) / 2;
+            } else {
+                y = (y - 1) / 2;
+            }
+        }
+
+        return [x, y];
     }
 
 }
 
-let m = new Maze(5, 7);
+let givenLength = 5;
+let givenWidth = 7;
+let m = new Maze(2 * givenLength + 1, 2 * givenWidth + 1);
 m.createMaze();
