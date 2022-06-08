14ef6c875de869cd4bea1901f5c50759541adf73
Eylul Oktay <gulzadeoktay@gmail.com>
2022-06-08 13:56:07 -0400

    js files

--- eventlisteners.js
+++ eventlisteners.js
@@ -1,8 +1,22 @@
 "use strict";
 let player = new Player(canvas.width / 2, canvas.height - 20);
+actorList.addActor(player);
 document.addEventListener("keydown", function (event) {
+    event.preventDefault();
     if (event.key === "ArrowLeft")
         player.moveLeft();
     if (event.key === "ArrowRight")
         player.moveRight();
+    if (event.key === "ArrowUp")
+        player.moveUp();
+    if (event.key === "ArrowDown")
+        player.moveDown();
 });
+// document.addEventListener("keyup", function(event: KeyboardEvent)){
+//     event.preventDefault()
+//     let elm = document.querySelector(`${event.key}`) as HTMLElement | null
+//     if (!elm) {
+//         return;
+//     }
+//     elm.classList.remove("player");
+// }
