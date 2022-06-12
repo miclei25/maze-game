1ae64eb6817ad501b036f22e6d3704fe8bda27c2
anaajesh <anaghaajesh2010@gmail.com>
2022-06-12 16:23:37 -0400

    hardcoded walls for testing  - ignore

---
+++ walls_hardcode.js
@@ -0,0 +1,42 @@
+var Walls = /** @class */ (function () {
+    function Walls() {
+        this.walls = [];
+        var bp = new Point(0, 100);
+        var ep = new Point(400, 100);
+        var wall = new Wall(bp, ep);
+        this.walls.push(wall);
+        bp = new Point(200, 200);
+        ep = new Point(500, 200);
+        wall = new Wall(bp, ep);
+        this.walls.push(wall);
+        bp = new Point(0, 300);
+        ep = new Point(450, 300);
+        wall = new Wall(bp, ep);
+        this.walls.push(wall);
+    }
+    Walls.prototype.draw = function () {
+        ctx.beginPath();
+        ctx.lineWidth = 10;
+        for (var _i = 0, _a = this.walls; _i < _a.length; _i++) {
+            var wall = _a[_i];
+            ctx.moveTo(wall.start_point.x, wall.start_point.y);
+            ctx.lineTo(wall.end_point.x, wall.end_point.y);
+        }
+        ctx.closePath();
+    };
+    return Walls;
+}());
+var Point = /** @class */ (function () {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    return Point;
+}());
+var Wall = /** @class */ (function () {
+    function Wall(start_point, end_point) {
+        this.start_point = start_point;
+        this.end_point = end_point;
+    }
+    return Wall;
+}());
