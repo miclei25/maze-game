014b6214ff93114e74b1002141f44ebacf89214d
Juni Myung <myung.yaejun@gmail.com>
2022-06-13 12:01:00 -0400

    changed starting position

--- eventlisteners.js
+++ eventlisteners.js
@@ -1,5 +1,5 @@
 "use strict";
-let player = new Player(canvas.width / 9.8, canvas.height - 20);
+let player = new Player(canvas.width / 9.7, canvas.height - 20);
 actorList.addActor(player);
 document.addEventListener("keydown", function (event) {
     event.preventDefault();
